4f775ec99c6cfcebf1221a4df2df2599f4ac533f
Sabhari Karthik <30544987+sabharikarthik@users.noreply.github.com>
2019-10-16 21:34:11 +0530

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
 require 'simplecov'
-SimpleCov.start do
-  formatter = SimpleCov::Formatter::CSVFormatter
-end
+SimpleCov.start
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
