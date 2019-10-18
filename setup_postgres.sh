2a0cc0c0bb2d1d833f4fef804240e7907f30fbd6
Sabhari Karthik <30544987+sabharikarthik@users.noreply.github.com>
2019-10-18 07:24:42 +0530

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -10,6 +10,9 @@ while getopts "u:p" opt; do
   esac
 done
 
+echo $DB_USERNAME
+echo $DB_PASSWORD
+
 sudo apt-get install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
