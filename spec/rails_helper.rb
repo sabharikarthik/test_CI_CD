23ca8670abaa7d0d5e6a015c25a59e0516ee23ec
Sabhari Karthik <30544987+sabharikarthik@users.noreply.github.com>
2019-10-18 07:46:51 +0530

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  minimum_coverage 60
+end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
