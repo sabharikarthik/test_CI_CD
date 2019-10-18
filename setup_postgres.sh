d9e0ee37c303d064f02b3f1d868c05b9f678de65
Sabhari Karthik <30544987+sabharikarthik@users.noreply.github.com>
2019-10-18 06:04:27 +0530

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -10,12 +10,10 @@ while getopts "u:p" opt; do
   esac
 done
 
-CREATE_ROLE_QUERY="psql -c \"CREATE ROLE ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}' LOGIN SUPERUSER;\""
-
 sudo apt-get install wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 sudo apt-get install libpq-dev
-sudo -u postgres bash -c $CREATE_ROLE_QUERY
+sudo -u postgres bash -c "psql -c \"CREATE ROLE ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}' LOGIN SUPERUSER;\""
